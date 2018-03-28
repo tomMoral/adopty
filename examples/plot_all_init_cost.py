d9a79ff7d6efe7cf5e58b46d6894d6516588c4ff
Thomas Moreau <toma.moral@gmail.com>
2018-03-28 15:24:52 +0200

    EXP add an example

---
+++ examples/plot_all_init_cost.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from adopty.ista import ista
+
+
+if __name__ == "__main__":
+
+    n_trials = 1000
+    n_atoms = 100
+    n_dimensions = 64
+    reg = .5
+
+    n_iters = 100
+
+    random_state = 42
+
+    rng = np.random.RandomState(random_state)
+
+    # Generate a problem
+    D = rng.randn(n_atoms, n_dimensions)
+    z = rng.randn(n_trials, n_atoms)
+    x = z.dot(D)
+
+    z_hat, cost_ista, _ = ista(D, x, reg, max_iter=100)
+
+    cost_ista = np.array(cost_ista)
+    plt.loglog(cost_ista - cost_ista.min() + 1e-16)
+    plt.show()
