FIx cost computation

adopty/ista.py
@@ -40,7 +40,7 @@ def ista(D, x, reg, z_init=None, max_iter=100):
     lmbd = lmbd_max * reg
 
     L = np.linalg.norm(D.dot(D.T), 2)
-    step_size = .99 / L
+    step_size = 1 / L
 
     # Generate an initial point
     if z_init:
@@ -49,7 +49,7 @@ def ista(D, x, reg, z_init=None, max_iter=100):
         z_hat = np.zeros((n_trials, n_atoms))
 
     times = []
-    cost_ista = []
+    cost_ista = [cost(z_hat, D, x, lmbd)]
     for _ in range(max_iter):
         t_start_iter = time()
         z_hat -= step_size * (z_hat.dot(D) - x).dot(D.T)
@@ -58,4 +58,4 @@ def ista(D, x, reg, z_init=None, max_iter=100):
 
         cost_ista += [cost(z_hat, D, x, lmbd)]
 
-    return z_hat, cost_ista, times
+    return z_hat, cost_ista, times, lmbd

adopty/lista.py
@@ -41,16 +41,20 @@ class Lista(torch.nn.Module):
             self.params += [(Wz, Wx)]
 
     def forward(self, x, lmbd, z0=None):
-        # Construct the first layer
+        # Compat numpy
+        if isinstance(x, np.ndarray):
+            x = torch.autograd.Variable(torch.Tensor(x).double())
+
+        # Compute the first layer
         p = self.params[0]
         z_hat = x.matmul(p[1])
         if z0:
             z_hat += z0.matmul(p[0])
         z_hat = torch.nn.functional.softshrink(z_hat, lmbd / self.L)
 
-        # Construct the following layers
+        # Compute the following layers
         for p in self.params[1:]:
             z_hat = z_hat.matmul(p[0]) + x.matmul(p[1])
             z_hat = torch.nn.functional.softshrink(z_hat, lmbd / self.L)
 
-        return x
+        return z_hat

adopty/tests/test_ista.py
@@ -4,6 +4,7 @@ from scipy import optimize
 
 
 from adopty.ista import ista
+from adopty.lista import Lista
 from adopty.utils import cost, grad
 from adopty.tests.utils import gradient_checker
 
@@ -26,9 +27,37 @@ def test_ista(reg):
     z = rng.randn(n_trials, n_atoms)
     x = z.dot(D)
 
-    z_hat, cost_ista, _ = ista(D, x, reg, max_iter=n_iters)
+    z_hat, cost_ista, *_ = ista(D, x, reg, max_iter=n_iters)
 
     assert all(np.diff(cost_ista) <= 0)
+
+
+@pytest.mark.parametrize('reg', [.1, .3, .5, 2])
+def test_lista(reg):
+
+    n_trials = 1000
+    n_atoms = 100
+    n_dimensions = 64
+
+    n_iters = 100
+
+    n_layers = 10
+    random_state = 42
+
+    rng = np.random.RandomState(random_state)
+
+    # Generate a problem
+    D = rng.randn(n_atoms, n_dimensions)
+    z = rng.randn(n_trials, n_atoms)
+    x = z.dot(D)
+
+    z_hat, cost_ista, _, lmbd = ista(D, x, reg, max_iter=n_layers)
+
+    lista = Lista(D, n_layers)
+    z_lista = lista(x, lmbd)
+
+    z_lista = z_lista.data.numpy()
+    assert np.isclose(cost_ista[n_layers], cost(z_lista, D, x, lmbd))
 
 
 def test_grad():