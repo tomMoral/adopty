fc9fcd4e86a227e4b64549655b99d62b088783fe
Pierre Ablin <pierreablin@gmail.com>
2019-03-07 09:52:17 +0100

    split function

--- adopty/autograd/lista.py
+++ adopty/autograd/lista.py
@@ -1,40 +1,13 @@
 import autograd.numpy as np
-from autograd import grad
 
+from autograd import grad
 from sklearn.utils import check_random_state
+
+from functions import lasso_loss, logreg_loss, lasso_der, logreg_der
 
 
 def st(x, l):
     return np.sign(x) * np.maximum(np.abs(x) - l, 0)
-
-
-def lasso_loss(z, x, D, lbda):
-    shape = x.shape
-    if len(shape) == 2:
-        _, n_samples = shape
-    else:
-        n_samples = 1
-    return (0.5 * np.sum((x - np.dot(D, z)) ** 2) +
-            lbda * np.sum(np.abs(z))) / n_samples
-
-
-def lasso_der(x, y):
-    return x - y
-
-
-def logreg_loss(z, x, D, lbda):
-    shape = x.shape
-    if len(shape) == 2:
-        _, n_samples = shape
-    else:
-        n_samples = 1
-    return (np.sum(np.log1p(np.exp(- x * np.dot(D, z)))) +
-            lbda * np.sum(np.abs(z))) / n_samples
-
-
-def logreg_der(x, y):
-    exp = np.exp(x * y)
-    return -y / (1. + exp)
 
 
 def _forward(x, weights, p, n_layers, D, level, der_function):
@@ -46,6 +19,29 @@ def _forward(x, weights, p, n_layers, D, level, der_function):
     for W in weights:
         z = st(z - np.dot(W, der_function(np.dot(D, z), x)), level)
     return z
+
+
+def sgd(weights, X, gradient_function, full_loss, logger, l_rate=0.001,
+        max_iter=100, batch_size=None, log=True, verbose=False):
+    _, n_samples = X.shape
+    if batch_size is None:
+        batch_size = n_samples
+    for i in range(max_iter):
+        sl = np.arange(i * batch_size, (i + 1) * batch_size) % n_samples
+        x = X[:, sl]
+        gradients = gradient_function(weights, x)
+        if i % 100 == 0 and (log or verbose):
+            loss_value = full_loss(weights, X)
+            gradient_value = np.sum((np.linalg.norm(g) for g in gradients))
+            if log:
+                logger['loss'].append(loss_value)
+                logger['grad'].append(gradient_value)
+            if verbose:
+                print('it %d, loss = %.2e, grad = %.2e' %
+                      (i, loss_value, gradient_value))
+        for weight, gradient in zip(weights, gradients):
+            weight -= l_rate * gradient
+    return weights
 
 
 class LISTA(object):
@@ -67,7 +63,6 @@ class LISTA(object):
         self.loss = loss
         self.der_function = der_function
         self.weights = [self.D.T / self.L, ] * n_layers  # Init weights
-        self.l_star = l_star
         self.logger = {}
         self.logger['loss'] = []
         self.logger['grad'] = []
@@ -76,32 +71,16 @@ class LISTA(object):
         return _forward(x, self.weights, self.p, self.n_layers, self.D,
                         self.level, self.der_function)
 
-    def fit(self, X, l_rate=1e-5, max_iter=100, batch_size=3, verbose=False,
-            log=True):
-        _, n_samples = X.shape
+    def fit(self, X, solver='sgd', *args, **kwargs):
 
         def full_loss(weights, x):
             z = _forward(x, weights, self.p, self.n_layers, self.D, self.level,
                          self.der_function)
-
             return self.loss(z, x, self.D, self.lbda)
 
         gradient_function = grad(full_loss)
-        weights = self.weights
-        for i in range(max_iter):
-            sl = np.arange(i * batch_size, (i + 1) * batch_size) % n_samples
-            x = X[:, sl]
-            gradients = gradient_function(weights, x)
-            if i % 100 == 0 and (log or verbose):
-                loss_value = full_loss(weights, X)
-                gradient_value = np.sum((np.linalg.norm(g) for g in gradients))
-                if log:
-                    self.logger['loss'].append(loss_value)
-                    self.logger['grad'].append(gradient_value)
-                if verbose:
-                    print('it %d, loss = %.2e, grad = %.2e' %
-                          (i, loss_value - self.l_star, gradient_value))
-            for weight, gradient in zip(weights, gradients):
-                weight -= l_rate * gradient
+        if solver == 'sgd':
+            weights = sgd(self.weights, X, gradient_function, full_loss,
+                          self.logger, *args, **kwargs)
         self.weights = weights
         return self
