FIx remove the scaling by n_samples

adopty/fista.py
@@ -54,7 +54,7 @@ def fista(D, x, lmbd, z_init=None, max_iter=100, tol=1e-8):
 
         z_hat_aux = z_hat
         y_hat -= step_size * grad(y_hat, D, x)
-        z_hat = soft_thresholding(y_hat, lmbd * step_size / n_samples)
+        z_hat = soft_thresholding(y_hat, lmbd * step_size)
         diff = z_hat - z_hat_aux
 
         tk_new = (1 + np.sqrt(1 + 4 * tk * tk)) / 2

adopty/lista.py
@@ -58,7 +58,7 @@ class Lista(torch.nn.Module):
                 parameters_layer = parameters_init[i]
             else:
                 if self.parametrization == "lista":
-                    parameters_layer = [self.B / self.L,
+                    parameters_layer = [I_k - self.B / self.L,
                                         self.D.T / self.L]
                 elif self.parametrization == "coupled":
                     parameters_layer = [self.D.T / self.L]
@@ -86,36 +86,35 @@ class Lista(torch.nn.Module):
                              "output_layer={} (n_layers={})"
                              .format(output_layer, self.n_layers))
 
-        n_samples = x.shape[0]
         z_hat = z0
         # Compute the following layers
         for p in self.params[:output_layer]:
             if self.parametrization == "lista":
                 if z_hat is None:
-                    z_hat = x.matmul(p[1]) / n_samples
+                    z_hat = x.matmul(p[1])
                 else:
-                    z_hat = z_hat - (z_hat.matmul(p[0]) -
-                                     x.matmul(p[1])) / n_samples
+                    z_hat = z_hat.matmul(p[0]) + x.matmul(p[1])
             elif self.parametrization == "coupled":
                 if z_hat is None:
-                    z_hat = x.matmul(p[0]) / n_samples
+                    z_hat = x.matmul(p[0])
                 else:
                     res = z_hat.matmul(self.D_) - x
-                    z_hat = z_hat - res.matmul(p[0]) / n_samples
+                    z_hat = z_hat - res.matmul(p[0])
             elif self.parametrization == "hessian":
                 if z_hat is None:
-                    z_hat = x.matmul(self.D_.t()).matmul(p[0]) / n_samples
+                    z_hat = x.matmul(self.D_.t()).matmul(p[0])
                 else:
                     grad = (z_hat.matmul(self.D_) - x).matmul(self.D_.t())
-                    z_hat = z_hat - grad.matmul(p[0]) / n_samples
+                    z_hat = z_hat - grad.matmul(p[0])
             else:
                 raise NotImplementedError()
-            z_hat = torch.nn.functional.softshrink(
-                z_hat, lmbd / (self.L * n_samples))
+            z_hat = torch.nn.functional.softshrink(z_hat, lmbd / self.L)
 
         return z_hat
 
     def fit(self, x, lmbd):
+        # Compat numpy
+        x = check_tensor(x, device=self.device)
 
         if self.verbose > 1:
             # compute fix point
@@ -128,18 +127,10 @@ class Lista(torch.nn.Module):
 
         training_loss = []
 
-        # Compat numpy
-        n_samples = x.shape[0]
-        x = check_tensor(x, device=self.device)
-
-        def loss_fn(z_hat):
-            res = z_hat.matmul(self.D_) - x
-            return (0.5 * (res * res).sum() +
-                    lmbd * torch.abs(z_hat).sum()) / n_samples
-
-        max_iter_per_layer = self.max_iter // self.n_layers
-        max_iter_per_layer = np.diff(np.logspace(0, np.log10(self.max_iter), 4,
-                                                 dtype=int)) + 1000
+        max_iter_per_layer = [self.max_iter // self.n_layers] * self.n_layers
+        # max_iter_per_layer = np.diff(
+        #     np.logspace(0, np.log10(self.max_iter), self.n_layers + 1,
+        #                 dtype=int)) + 1000
 
         for n_layer in range(1, self.n_layers + 1):
             lr = 1
@@ -149,11 +140,11 @@ class Lista(torch.nn.Module):
                 # Compute the forward operator
                 self.zero_grad()
                 z_hat = self(x, lmbd, output_layer=n_layer)
-                loss = loss_fn(z_hat)
-                loss_val = loss.detach().numpy()
+                loss = self.loss_fn(x, lmbd, z_hat)
 
                 # Verbosity of the output
                 if self.verbose > 5 and i % 10 == 0:
+                    loss_val = loss.detach().cpu().numpy()
                     print(i, loss_val - c_star)
                 elif self.verbose > 0 and i % 50 == 0:
                     print("\rFitting model (layer {}/{}): {:7.2%}"
@@ -162,24 +153,35 @@ class Lista(torch.nn.Module):
                           end="", flush=True)
 
                 # Back-tracking line search
-                if len(training_loss) > 0 and training_loss[-1] < loss_val:
+                if len(training_loss) > 0 and torch.le(training_loss[-1],
+                                                       loss):
                     if i + 1 == max_iter:
+                        # In this case, do not perform the last step
                         lr *= 2
                     lr = self.backtrack_parameters(parameters, lr)
                     continue
 
                 # Accepting the previous point
-                training_loss.append(loss_val)
+                training_loss.append(loss)
 
                 # Next gradient iterate
                 loss.backward()
                 lr = self.update_parameters(parameters, lr=lr)
 
-        self.training_loss_ = np.array(training_loss)
+        self.training_loss_ = np.array([loss.detach().cpu().numpy()
+                                        for loss in training_loss])
         print("\rFitting model: done".ljust(80))
 
+    def loss_fn(self, x, lmbd, z_hat):
+        n_samples = x.shape[0]
+        x = check_tensor(x, device=self.device)
+
+        res = z_hat.matmul(self.D_) - x
+        return (0.5 * (res * res).sum() +
+                lmbd * torch.abs(z_hat).sum()) / n_samples
+
     def update_parameters(self, parameters, lr):
-        lr = min(2 * lr, 1e6)
+        lr = min(4 * lr, 1e6)
         self._saved_gradient = []
         for p in parameters:
             if p.grad is None:
@@ -203,6 +205,12 @@ class Lista(torch.nn.Module):
             p.data.add_(lr, g)
         return lr
 
-    def transform(self, x, lmbd, z0=None):
+    def transform(self, x, lmbd, z0=None, output_layer=None):
         with torch.no_grad():
-            return self(x, lmbd, z0=z0)
+            return self(x, lmbd, z0=z0,
+                        output_layer=output_layer).cpu().numpy()
+
+    def score(self, x, lmbd, z0=None):
+        x = check_tensor(x, device=self.device)
+        with torch.no_grad():
+            return self.loss_fn(x, lmbd, self(x, lmbd, z0=z0)).cpu().numpy()

adopty/tests/test_solvers.py
@@ -28,7 +28,7 @@ def test_ista(reg):
 
     z_hat, cost_ista, *_ = ista(D, x, reg, max_iter=n_iters)
 
-    assert all(np.diff(cost_ista) < 0)
+    assert all(np.diff(cost_ista) <= 0)
     assert all(np.diff(cost_ista) <= 1e-8)
 
 
@@ -50,7 +50,7 @@ def test_fista(reg):
     z_hat_fista, cost_fista, *_ = fista(D, x, reg, max_iter=n_iters)
 
     assert np.isclose(cost_ista[1], cost_fista[1])
-    assert cost_ista[-1] > cost_fista[-1]
+    assert np.isclose(cost_ista[-1], cost_fista[-1])
 
     diff = z_hat_fista - z_hat_ista
     print(diff[abs(diff) > 1e-2])
@@ -104,7 +104,7 @@ def test_grad():
 
     n_dim = 20
     n_atoms = 10
-    n_samples = 10
+    n_samples = 1
     random_state = 42
 
     # Generate a problem

examples/plot_all_cost.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 from adopty.lista import Lista
@@ -6,43 +7,60 @@ from adopty.datasets import make_coding
 from adopty.loss_and_gradient import cost_lasso
 
 
-def get_c_star(x, D, z, reg, n_iter=10000):
-    ista = Lista(D, n_iter, parametrization="coupled")
-    z_star = ista.transform(x, reg, z0=z).numpy()
-    return cost_lasso(z_star, D, x, reg)
+def get_c_star(x, D, z, reg, n_iter=10000, device=None):
+    ista = Lista(D, n_iter, parametrization="coupled", device=device)
+    return ista.score(x, reg, z0=z)
 
 
-x, D, z = make_coding(n_samples=1000)
-reg = .5
-n_layers = 3
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser('')
+    parser.add_argument('--gpu', action='store_true',
+                        help='Use GPU for training')
+    args = parser.parse_args()
 
-x_test = np.random.randn(*x.shape)
+    device = 'cuda' if args.gpu else 'cpu'
 
+    n_dim = 2
+    n_atoms = 8
+    n_samples = 10000
 
-format_cost = "{}: {} cost = {:.3e}"
-c_star = get_c_star(x, D, z, reg)
+    x, D, z = make_coding(n_samples=n_samples, n_atoms=n_atoms, n_dim=n_dim)
+    reg = .7
+    n_layers = 3
 
+    x_test = np.random.randn(*x.shape)
 
-saved_model = {}
-for parametrization in ['hessian', 'coupled']:
-    lista = Lista(D, n_layers, parametrization=parametrization, max_iter=3000)
-    z_hat_test = lista.transform(x_test, reg).numpy()
-    c_star_test = get_c_star(x_test, D, z_hat_test, reg)
-    cost_test = cost_lasso(z_hat_test, D, x_test, reg) - c_star_test
-    print(format_cost.format("Un-trained[{}]".format(parametrization), "test",
-                             cost_test))
-    lista.fit(x, reg)
-    z_hat_test = lista.transform(x_test, reg).numpy()
-    c_star_test = get_c_star(x_test, D, z_hat_test, reg)
-    cost_test = cost_lasso(z_hat_test, D, x_test, reg) - c_star_test
-    print(format_cost.format("Trained[{}]".format(parametrization), "test",
-                             cost_test))
-    saved_model[parametrization] = lista
+    format_cost = "{}: {} cost = {:.3e}"
+    c_star = get_c_star(x, D, z, reg, device=device)
 
-    z_hat = lista.transform(x, reg).numpy()
-    c_star = min(c_star, get_c_star(x, D, z_hat, reg))
-    plt.loglog(lista.training_loss_ - c_star, label=parametrization)
+    saved_model = {}
+    for parametrization in ['hessian', 'coupled']:
+        lista = Lista(D, n_layers, parametrization=parametrization,
+                      max_iter=3000, device=device)
 
-plt.legend()
+        z_hat_test = lista.transform(x_test, reg)
+        c_star_test = get_c_star(x_test, D, z_hat_test, reg, device=device)
+        cost_test = cost_lasso(z_hat_test, D, x_test, reg) - c_star_test
+        print(format_cost.format("Un-trained[{}]".format(parametrization),
+                                 "test", cost_test))
 
-plt.show()
+        # Train and evaluate the network
+        lista.fit(x, reg)
+        z_hat_test = lista.transform(x_test, reg)
+        c_star_test = get_c_star(x_test, D, z_hat_test, reg, device=device)
+        cost_test = cost_lasso(z_hat_test, D, x_test, reg) - c_star_test
+        print(format_cost.format("Trained[{}]".format(parametrization),
+                                 "test", cost_test))
+        saved_model[parametrization] = lista
+
+        z_hat = lista.transform(x, reg)
+        c_star = min(c_star, get_c_star(x, D, z_hat, reg, device=device))
+        plt.loglog(lista.training_loss_ - c_star, label=parametrization)
+
+    plt.legend()
+
+    # Save the figure if it is not displayed
+    if mpl.get_backend() == 'agg':
+        plt.savefig("output.pdf", dpi=300)
+    plt.show()