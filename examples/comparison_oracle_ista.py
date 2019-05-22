cf9f2a4e85e2de897a0cc4a8887ce08c511c3d76
Pierre Ablin <pierreablin@gmail.com>
2019-05-23 01:01:56 +0200

    ENH figs

--- examples/comparison_oracle_ista.py
+++ examples/comparison_oracle_ista.py
@@ -33,7 +33,7 @@ z_hat, c_star, _ = ista(D, x[None, :], lmbd, max_iter=3000)
 c_star = c_star[-1]
 
 
-f, axes = plt.subplots(2, 1, figsize=(4, 3), sharex=True,
+f, axes = plt.subplots(2, 1, figsize=(4, 2.5), sharex=True,
                        gridspec_kw={'height_ratios': [3, 1]})
 ax = axes[0]
 ax.semilogy(cost_ista - c_star, label=r'ISTA', color=colors['ISTA'],
@@ -50,18 +50,19 @@ lgd = f.legend(ncol=3, loc='upper center', handletextpad=0.1, handlelength=0.9,
                columnspacing=.7)
 ax2 = axes[1]
 ax2.plot(steps / steps[0], color=colors['OISTA'], linewidth=2)
-textx = len(steps) / 2 - 3
-text_width = 10
-ax2.hlines(1., 0, textx - 4, color='k', linestyle='--')
+textx = len(steps) / 2 - 10
+text_width = 24
+ax2.hlines(1., 0, textx - 2, color='k', linestyle='--')
 ax2.hlines(1., textx + text_width, len(steps), color='k', linestyle='--')
-ax2.text(textx, .7, r'$\frac{1}{L}$', fontsize=15)
+ax2.text(textx, .7, r'$1/L$', fontsize=15)
 ax2.set_ylim(0.1, max(steps/steps[0]) + .2)
-ax2.set_ylabel(r'Oracle step')
+ax2.set_ylabel('Oracle \n step')
 ax2.set_xlim(0, len(steps)-1)
+ax2.set_yticks([1, 3])
 x = ax2.set_xlabel(r'Number of iterations')
 y = ax.set_ylabel(r'$F_x - F_x^*$')
-ax2.grid()
-plt.subplots_adjust(top=0.85)
+# ax2.yaxis.grid(True)
+plt.subplots_adjust(top=0.82)
 plt.savefig('examples/figures/comparison_oista_ista.pdf',
             bbox_extra_artists=[lgd, x, y], bbox_inches='tight')
 # plt.show()
