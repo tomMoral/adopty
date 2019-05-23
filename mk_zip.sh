bdd1a7133025de184158ede46924516605f69e7c
tomMoral <thomas.moreau.2010@gmail.com>
2019-05-23 13:52:54 +0200

    ENH improve mk_zip.sh

--- mk_zip.sh
+++ mk_zip.sh
@@ -1,3 +1,14 @@
+BAD_WORDS="thomas\\|moreau\\|ablin\\|pierre\\|alexandre\\|gramfort\\|inria"
+
+unsafe=$(git grep -i -- "$BAD_WORDS" ":(exclude)mk_zip.sh")
+if [[ ! -z $unsafe ]]; then
+	echo -e '\033[1;31mThe repository is not anonymous!\n\n\033[0m'
+	echo -e 'Found:\n'$unsafe
+else
+	echo -e '\033[1;32mThe repository seems anonymous.\0033[0m\n\n'
+fi
+
+
 
 # Check that all included examples are described in the README.md
 for f in examples/*.py; do
@@ -11,7 +22,7 @@ done
 
 # Get all files in the repository and avoid includingn git files
 echo -e '\n======  \033[1;33mCreate the archive\033[0m =======\n'
-list_files=$(git ls-tree -r --name-only anonymous | grep -v .git)
+list_files=$(git ls-tree -r --name-only anonymous | grep -v ".git\\|mk_zip" )
 zip adopty.zip $list_files
 
 # Display the contents of the zip
